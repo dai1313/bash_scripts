4a4585adffcbe7361607a61799f550e699498ae0
James Murphy <jmurphy4@mail.umw.edu>
2015-03-09 16:54:53 -0400

    Trackpad scripts created for keybinds

---
+++ trackpad_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xinput set-prop 12 "Device Enabled" 0
+notify-send "Trackpad deactivated."
+exit
---
+++ trackpad_on.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xinput set-prop 12 "Device Enabled" 1
+notify-send "Trackpad activated."
+exit
